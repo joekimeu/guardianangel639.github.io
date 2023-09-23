cccff05b0b632259bdb7f477dc5c141383995bd2
guardianangel639 <123139768+guardianangel639@users.noreply.github.com>
2023-09-23 13:40:10 -0400

    updates login page

--- Scripts/login.js
+++ Scripts/login.js
@@ -14,23 +14,40 @@ navLinks.forEach(link => {
 })
 
 //login password information
-const username = document.getElementById('username')
-const password = document.getElementById('password')
-const form = document.getElementById('form')
-const errorElement = document.getElementById('error')
+const username = document.getElementById('username');
+const password = document.getElementById('password');
+const form = document.getElementById('form');
+const errorElement = document.getElementById('error');
+
 
 form.addEventListener('submit', (e) => {
-    let messages = []
-    if (username.value === '' || username.value == null) {
-        messages.push('Username is required')
+    let messages = [];
+    if (username.value != 'gahaemployee' && password.value != 'thankyouguys') {
+        messages.push('Incorrect Username and Password');
+        username.style.border ="3px solid red";
+        password.style.border ="3px solid red";
+        username.focus();
+        password.focus()
     }
 
-    if (password.value === '' || password.value == null) {
-        messages.push('Username is required')
-    }
-
+    else if (password.value != 'thankyouguys') {
+        messages.push('Incorrect Password');
+        username.style.border = "";
+        password.style.border ="3px solid red";
+        password.focus();
+    } 
+  
+    else if (username.value != 'gahaemployee') {
+        messages.push('Incorrect Username');
+        username.style.border ="3px solid red";
+        password.style.border = "";
+        username.focus();
+    } 
+    
     if (messages.length > 0) {
-        e.preventDefault()
-        errorElement.innerText = messages.join(', ')
+        e.preventDefault();
+        errorElement.innerText = messages.join(', ');
+        messages.style.border = "3px solid red";
+        messages.style.display = "block";
     }
 })
